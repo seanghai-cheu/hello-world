0615d37ead0cf02fa5832728adb43633ddaef7ae
malinka <phann.malinka@apdbank.com.kh>
2022-03-15 01:14:09 +0700

    upload

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="full">
+      <Nuxt />
+    </div>
+</template>
+
+<style scoped>
+.full {
+  height: 100vh;
+}
+</style>
+
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="here">
+      hello world
+  </div>
+</template>
+
+<style scoped>
+  .here {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
